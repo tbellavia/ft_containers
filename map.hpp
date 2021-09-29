f45e47623477a1da8889963fa13b14cd10870714
Tony <com.bellavi@tutanota.com>
2021-09-29 04:07:12 +0200

    Add : Adding a private implementation of rb node
    	map :
    		- rb_node :
    			- Constructors :
    				- default constructor
    				- data constructor
    				- data-parent constructor
    
    			- Access member functions :
    				- sibling
    				- grand_parent
    				- uncle
    		- create_node member function

--- map.hpp
+++ map.hpp
@@ -14,6 +14,12 @@ namespace ft
 		class Alloc = std::allocator<ft::pair<const Key, T> >	// map::allocator_type
 	>
 	class map {
+		private:
+			/**
+			 * Forward declarations.
+			 * 
+			 */
+			struct rb_node;
 		public:
 			typedef Key										key_type;
 			typedef T										mapped_type;
@@ -28,6 +34,135 @@ namespace ft
 			typedef allocator_type::const_pointer			const_pointer;
 			typedef std::ptrdiff_t							difference_type;
 			typedef std::size_t								size_type;
+
+
+		/**
+		 * Private declarations.
+		 * 
+		 */
+		private:
+			rb_node					*m_root;
+			allocator_type			m_alloc;
+			rb_node::allocator_type m_rb_alloc;
+
+		/**
+		 * Private implementations.
+		 * 
+		 */
+		private:
+			rb_node *create_node(){
+				rb_node *node = m_rb_alloc.allocate( 1 );
+
+				m_rb_alloc.construct( node );
+				return node;
+			}
+
+			rb_node *create_node( const value_type &data ){
+				rb_node *node = m_rb_alloc.allocate( 1 );
+
+				m_rb_alloc.construct( node, data );
+				return node;
+			}
+
+			rb_node *create_node( const value_type &data, rb_node *parent ){
+				rb_node *node = m_rb_alloc.allocate( 1 );
+
+				m_rb_alloc.construct( node, data, parent );
+				return node;
+			}
+
+			enum rb_color { BLACK = 1, RED };
+			/**
+			 * Internal struct representing a binary tree node.
+			 * 
+			 */
+			struct rb_node {
+				value_type	data;
+				rb_node		*parent;
+				rb_node		*left;
+				rb_node		*right;
+				rb_color	color;
+
+				/**
+				 * Default constructor
+				 * 
+				 * Constructs a new empty node with default value_type() and all
+				 * pointers to nullptr.
+				 * 
+				 * Color is always set to RED as it will always be inserted as a RED
+				 * node.
+				 * 
+				 */
+				rb_node() 
+					: data( value_type() ), parent( nullptr ), left( nullptr ), right( nullptr ), color( rb_color::RED ) { }
+
+				/**
+				 * Data constructor
+				 * 
+				 * Constructs a new node with data and all pointers to nullptr.
+				 * 
+				 * Color is always set to RED as it will always be inserted as a RED
+				 * node.
+				 * 
+				 */
+				rb_node( const value_type &__data )
+					: data( __data ), parent( nullptr ), left( nullptr ), right( nullptr ), color( rb_color::RED ) { }
+
+				/**
+				 * Data-Parent constructor
+				 * 
+				 * Constructs a new node with data and a parent, right and left 
+				 * pointers are set to nullptr.
+				 * 
+				 * Color is always set to RED as it will always be inserted as a RED
+				 * node.
+				 * 
+				 */
+				rb_node( const value_type &__data, rb_node *__parent )
+					: data( __data ), parent( __parent ), left( nullptr ), right( nullptr ), color( rb_color::RED ) { }
+
+				/**
+				 * Get the grand parent
+				 * 
+				 * Returns the grand parent of the current node.
+				 * 
+				 */
+				rb_node *grand_parent() {
+					if ( this->parent == nullptr )
+						return nullptr;
+					return this->parent->parent;
+				}
+
+				/**
+				 * Get uncle node
+				 * 
+				 * Returns the uncle node of the current node.
+				 * 
+				 */
+				rb_node *uncle() {
+					rb_node *gp = this->grand_parent();
+
+					if ( gp == nullptr )
+						return nullptr;
+					return this->parent->sibling();
+				}
+
+				/**
+				 * Get the sibling node
+				 * 
+				 * Returns the sibling node.
+				 * 
+				 */
+				rb_node *sibling() {
+					if ( this->parent == nullptr )
+						return nullptr;
+					if ( this == this->parent->left )
+						return this->parent->right;
+					return this->parent->left;
+				}
+
+				typedef std::allocator<rb_node> allocator_type;
+			};
 	};
 }
 
