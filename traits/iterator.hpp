8f728694942fa7bb8995ca32eb8e5e3a305ad6dd
bbellavi <bbellavi@student.42.fr>
2022-01-19 16:49:19 +0100

    WIP On iterator traits

--- traits/iterator.hpp
+++ traits/iterator.hpp
@@ -1,10 +1,23 @@
 #ifndef ITERATOR_HPP
 #define ITERATOR_HPP
 
+#include <iterator>
 #include "iterator/vector_iterator.hpp"
 #include "iterator/map_iterator.hpp"
 
 namespace ft {
+	// Remove const qualifier
+	template<typename T>
+	struct remove_cv {
+		typedef T	type;
+	};
+
+
+	template<typename T>
+	struct remove_cv<const T> {
+		typedef T	type;
+	};
+
 	/**
 	 * Iterator traits
 	 * 
@@ -36,7 +49,22 @@ namespace ft {
 	struct ft::iterator_traits<ft::map_::reverse_iterator<T> > : ft::true_type {};
 
 	template<typename T>
-	struct ft::iterator_traits<T*> : ft::true_type {};
+	struct ft::iterator_traits<T*> : ft::true_type {
+		typedef iterator::random_access_iterator_tag	iterator_category;
+		typedef T										value_type;
+		typedef ptrdiff_t								difference_type;
+		typedef T*										pointer;
+		typedef T&										reference;
+	};
+
+	template<typename T>
+	struct ft::iterator_traits<const T*> : ft::true_type {
+		typedef iterator::random_access_iterator_tag	iterator_category;
+		typedef T										value_type;
+		typedef ptrdiff_t								difference_type;
+		typedef const T*								pointer;
+		typedef const T&								reference;
+	};
 }
 
 #endif
