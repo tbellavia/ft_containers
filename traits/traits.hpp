354b7b660f7f6907be1d4ca3cea87fb9756d904a
bbellavi <bbellavi@student.42.fr>
2022-01-22 16:10:29 +0100

    Add: iterator_traits

--- traits/traits.hpp
+++ traits/traits.hpp
@@ -36,6 +36,38 @@ namespace ft {
 	 * Define a traits for iterators.
 	 * 
 	 */
+	template<typename Iterator>
+	struct iterator_traits {
+		typedef typename Iterator::iterator_category	iterator_category;
+		typedef typename Iterator::value_type			value_type;
+		typedef typename Iterator::difference_type		difference_type;
+		typedef typename Iterator::pointer				pointer;
+		typedef typename Iterator::reference			reference;
+	};
+
+	template<typename T>
+	struct iterator_traits<T*> {
+		typedef std::random_access_iterator_tag	iterator_category;
+		typedef T								value_type;
+		typedef ptrdiff_t						difference_type;
+		typedef T*								pointer;
+		typedef T&								reference;
+	};
+
+	template<typename T>
+	struct iterator_traits<const T*> {
+		typedef std::random_access_iterator_tag	iterator_category;
+		typedef T								value_type;
+		typedef ptrdiff_t						difference_type;
+		typedef const T*						pointer;
+		typedef const T&						reference;
+	};
+
+	/**
+	 * Is iterator
+	 * 
+	 * Define a way to check if it is iterator
+	 */
 	template<typename T>
 	struct is_iterator : ft::false_type { };
 }
